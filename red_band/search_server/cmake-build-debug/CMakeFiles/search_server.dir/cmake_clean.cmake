99a6949d021ecd102860e8c8967a1502e211c2c9
thekirjava <thekirjava@yandex.by>
2020-06-25 20:19:00 +0300

    ml start

---
+++ red_band/search_server/cmake-build-debug/CMakeFiles/search_server.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/search_server.dir/main.cpp.obj"
+  "CMakeFiles/search_server.dir/parse.cpp.obj"
+  "CMakeFiles/search_server.dir/search_server.cpp.obj"
+  "libsearch_server.dll.a"
+  "search_server.exe"
+  "search_server.exe.manifest"
+  "search_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/search_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
